a93cd4ed80b3260c349a9266febae45ffc1afe37
shravanrn <shravanrn@gmail.com>
2023-05-12 06:25:14 -0400

    msvc support

--- src/prebuilt/wasm2c_source_atomicops_declarations.cc
+++ src/prebuilt/wasm2c_source_atomicops_declarations.cc
@@ -1,7 +1,106 @@
-const char* s_source_atomicops_declarations = R"w2c_template(#if __STDC_VERSION__ >= 201112L
+const char* s_source_atomicops_declarations = R"w2c_template(#if defined(_MSC_VER)
 )w2c_template"
 R"w2c_template(
-#include <stdatomic.h>
+#include <intrin.h>
+)w2c_template"
+R"w2c_template(
+// Use MSVC intrinsics
+)w2c_template"
+R"w2c_template(
+// For loads and stores, its not clear if we can rely on register width loads
+)w2c_template"
+R"w2c_template(// and stores to be atomic as reported here
+)w2c_template"
+R"w2c_template(// https://learn.microsoft.com/en-us/windows/win32/sync/interlocked-variable-access?redirectedfrom=MSDN
+)w2c_template"
+R"w2c_template(// or if we have to reuse other instrinsics
+)w2c_template"
+R"w2c_template(// https://stackoverflow.com/questions/42660091/atomic-load-in-c-with-msvc
+)w2c_template"
+R"w2c_template(// We reuse other intrinsics to be cautious
+)w2c_template"
+R"w2c_template(#define atomic_load_u8(a, v) _InterlockedOr8(a, 0)
+)w2c_template"
+R"w2c_template(#define atomic_load_u16(a, v) _InterlockedOr16(a, 0)
+)w2c_template"
+R"w2c_template(#define atomic_load_u32(a, v) _InterlockedOr(a, 0)
+)w2c_template"
+R"w2c_template(#define atomic_load_u64(a, v) _InterlockedOr64(a, 0)
+)w2c_template"
+R"w2c_template(
+#define atomic_store_u8(a, v) _InterlockedExchange8(a, v)
+)w2c_template"
+R"w2c_template(#define atomic_store_u16(a, v) _InterlockedExchange16(a, v)
+)w2c_template"
+R"w2c_template(#define atomic_store_u32(a, v) _InterlockedExchange(a, v)
+)w2c_template"
+R"w2c_template(#define atomic_store_u64(a, v) _InterlockedExchange64(a, v)
+)w2c_template"
+R"w2c_template(
+#define atomic_add_u8(a, v) _InterlockedExchangeAdd8(a, v)
+)w2c_template"
+R"w2c_template(#define atomic_add_u16(a, v) _InterlockedExchangeAdd16(a, v)
+)w2c_template"
+R"w2c_template(#define atomic_add_u32(a, v) _InterlockedExchangeAdd(a, v)
+)w2c_template"
+R"w2c_template(#define atomic_add_u64(a, v) _InterlockedExchangeAdd64(a, v)
+)w2c_template"
+R"w2c_template(
+#define atomic_sub_u8(a, v) _InterlockedExchangeAdd8(a, -(v))
+)w2c_template"
+R"w2c_template(#define atomic_sub_u16(a, v) _InterlockedExchangeAdd16(a, -(v))
+)w2c_template"
+R"w2c_template(#define atomic_sub_u32(a, v) _InterlockedExchangeAdd(a, -(v))
+)w2c_template"
+R"w2c_template(#define atomic_sub_u64(a, v) _InterlockedExchangeAdd64(a, -(v))
+)w2c_template"
+R"w2c_template(
+#define atomic_and_u8(a, v) _InterlockedAnd8(a, v)
+)w2c_template"
+R"w2c_template(#define atomic_and_u16(a, v) _InterlockedAnd16(a, v)
+)w2c_template"
+R"w2c_template(#define atomic_and_u32(a, v) _InterlockedAnd(a, v)
+)w2c_template"
+R"w2c_template(#define atomic_and_u64(a, v) _InterlockedAnd64(a, v)
+)w2c_template"
+R"w2c_template(
+#define atomic_or_u8(a, v) _InterlockedOr8(a, v)
+)w2c_template"
+R"w2c_template(#define atomic_or_u16(a, v) _InterlockedOr16(a, v)
+)w2c_template"
+R"w2c_template(#define atomic_or_u32(a, v) _InterlockedOr(a, v)
+)w2c_template"
+R"w2c_template(#define atomic_or_u64(a, v) _InterlockedOr64(a, v)
+)w2c_template"
+R"w2c_template(
+#define atomic_xor_u8(a, v) _InterlockedXor8(a, v)
+)w2c_template"
+R"w2c_template(#define atomic_xor_u16(a, v) _InterlockedXor16(a, v)
+)w2c_template"
+R"w2c_template(#define atomic_xor_u32(a, v) _InterlockedXor(a, v)
+)w2c_template"
+R"w2c_template(#define atomic_xor_u64(a, v) _InterlockedXor64(a, v)
+)w2c_template"
+R"w2c_template(
+#define atomic_exchange_u8(a, v) _InterlockedExchange8(a, v)
+)w2c_template"
+R"w2c_template(#define atomic_exchange_u16(a, v) _InterlockedExchange16(a, v)
+)w2c_template"
+R"w2c_template(#define atomic_exchange_u32(a, v) _InterlockedExchange(a, v)
+)w2c_template"
+R"w2c_template(#define atomic_exchange_u64(a, v) _InterlockedExchange64(a, v)
+)w2c_template"
+R"w2c_template(
+#define atomic_compare_exchange_u8(a, expected_ptr, desired) _InterlockedCompareExchange8(a, desired, *(expected_ptr))
+)w2c_template"
+R"w2c_template(#define atomic_compare_exchange_u16(a, expected_ptr, desired) _InterlockedCompareExchange16(a, desired, *(expected_ptr))
+)w2c_template"
+R"w2c_template(#define atomic_compare_exchange_u32(a, expected_ptr, desired) _InterlockedCompareExchange(a, desired, *(expected_ptr))
+)w2c_template"
+R"w2c_template(#define atomic_compare_exchange_u64(a, expected_ptr, desired) _InterlockedCompareExchange64(a, desired, *(expected_ptr))
+)w2c_template"
+R"w2c_template(
+#define atomic_fence() _ReadWriteBarrier()
 )w2c_template"
 R"w2c_template(
 #else
@@ -9,29 +108,91 @@ R"w2c_template(
 R"w2c_template(
 // Use gcc/clang/icc intrinsics
 )w2c_template"
-R"w2c_template(#define atomic_load(a) __atomic_load_n(a, __ATOMIC_SEQ_CST)
+R"w2c_template(#define atomic_load_u8(a) __atomic_load_n((u8*) (a), __ATOMIC_SEQ_CST)
 )w2c_template"
-R"w2c_template(#define atomic_store(a, v) __atomic_store_n(a, v, __ATOMIC_SEQ_CST)
+R"w2c_template(#define atomic_load_u16(a) __atomic_load_n((u16*) (a), __ATOMIC_SEQ_CST)
 )w2c_template"
-R"w2c_template(#define atomic_fetch_add(a, v) __atomic_fetch_add(a, v, __ATOMIC_SEQ_CST)
+R"w2c_template(#define atomic_load_u32(a) __atomic_load_n((u32*) (a), __ATOMIC_SEQ_CST)
 )w2c_template"
-R"w2c_template(#define atomic_fetch_sub(a, v) __atomic_fetch_sub(a, v, __ATOMIC_SEQ_CST)
+R"w2c_template(#define atomic_load_u64(a) __atomic_load_n((u64*) (a), __ATOMIC_SEQ_CST)
 )w2c_template"
-R"w2c_template(#define atomic_fetch_and(a, v) __atomic_fetch_and(a, v, __ATOMIC_SEQ_CST)
+R"w2c_template(
+#define atomic_store_u8(a, v) __atomic_store_n((u8*) (a), v, __ATOMIC_SEQ_CST)
 )w2c_template"
-R"w2c_template(#define atomic_fetch_or(a, v) __atomic_fetch_or(a, v, __ATOMIC_SEQ_CST)
+R"w2c_template(#define atomic_store_u16(a, v) __atomic_store_n((u16*) (a), v, __ATOMIC_SEQ_CST)
 )w2c_template"
-R"w2c_template(#define atomic_fetch_xor(a, v) __atomic_fetch_xor(a, v, __ATOMIC_SEQ_CST)
+R"w2c_template(#define atomic_store_u32(a, v) __atomic_store_n((u32*) (a), v, __ATOMIC_SEQ_CST)
 )w2c_template"
-R"w2c_template(#define atomic_exchange(a, v) __atomic_exchange_n(a, v, __ATOMIC_SEQ_CST)
+R"w2c_template(#define atomic_store_u64(a, v) __atomic_store_n((u64*) (a), v, __ATOMIC_SEQ_CST)
 )w2c_template"
-R"w2c_template(#define atomic_compare_exchange_strong(a, expected_ptr, desired)         \
+R"w2c_template(
+#define atomic_add_u8(a, v) __atomic_fetch_add((u8*) (a), v, __ATOMIC_SEQ_CST)
 )w2c_template"
-R"w2c_template(  __atomic_compare_exchange_n(a, expected_ptr, desired, 0 /* is_weak */, \
+R"w2c_template(#define atomic_add_u16(a, v) __atomic_fetch_add((u16*) (a), v, __ATOMIC_SEQ_CST)
 )w2c_template"
-R"w2c_template(                              __ATOMIC_SEQ_CST, __ATOMIC_SEQ_CST)
+R"w2c_template(#define atomic_add_u32(a, v) __atomic_fetch_add((u32*) (a), v, __ATOMIC_SEQ_CST)
 )w2c_template"
-R"w2c_template(#endif
+R"w2c_template(#define atomic_add_u64(a, v) __atomic_fetch_add((u64*) (a), v, __ATOMIC_SEQ_CST)
+)w2c_template"
+R"w2c_template(
+#define atomic_sub_u8(a, v) __atomic_fetch_sub((u8*) (a), v, __ATOMIC_SEQ_CST)
+)w2c_template"
+R"w2c_template(#define atomic_sub_u16(a, v) __atomic_fetch_sub((u16*) (a), v, __ATOMIC_SEQ_CST)
+)w2c_template"
+R"w2c_template(#define atomic_sub_u32(a, v) __atomic_fetch_sub((u32*) (a), v, __ATOMIC_SEQ_CST)
+)w2c_template"
+R"w2c_template(#define atomic_sub_u64(a, v) __atomic_fetch_sub((u64*) (a), v, __ATOMIC_SEQ_CST)
+)w2c_template"
+R"w2c_template(
+#define atomic_and_u8(a, v) __atomic_fetch_and((u8*) (a), v, __ATOMIC_SEQ_CST)
+)w2c_template"
+R"w2c_template(#define atomic_and_u16(a, v) __atomic_fetch_and((u16*) (a), v, __ATOMIC_SEQ_CST)
+)w2c_template"
+R"w2c_template(#define atomic_and_u32(a, v) __atomic_fetch_and((u32*) (a), v, __ATOMIC_SEQ_CST)
+)w2c_template"
+R"w2c_template(#define atomic_and_u64(a, v) __atomic_fetch_and((u64*) (a), v, __ATOMIC_SEQ_CST)
+)w2c_template"
+R"w2c_template(
+#define atomic_or_u8(a, v) __atomic_fetch_or((u8*) (a), v, __ATOMIC_SEQ_CST)
+)w2c_template"
+R"w2c_template(#define atomic_or_u16(a, v) __atomic_fetch_or((u16*) (a), v, __ATOMIC_SEQ_CST)
+)w2c_template"
+R"w2c_template(#define atomic_or_u32(a, v) __atomic_fetch_or((u32*) (a), v, __ATOMIC_SEQ_CST)
+)w2c_template"
+R"w2c_template(#define atomic_or_u64(a, v) __atomic_fetch_or((u64*) (a), v, __ATOMIC_SEQ_CST)
+)w2c_template"
+R"w2c_template(
+#define atomic_xor_u8(a, v) __atomic_fetch_xor((u8*) (a), v, __ATOMIC_SEQ_CST)
+)w2c_template"
+R"w2c_template(#define atomic_xor_u16(a, v) __atomic_fetch_xor((u16*) (a), v, __ATOMIC_SEQ_CST)
+)w2c_template"
+R"w2c_template(#define atomic_xor_u32(a, v) __atomic_fetch_xor((u32*) (a), v, __ATOMIC_SEQ_CST)
+)w2c_template"
+R"w2c_template(#define atomic_xor_u64(a, v) __atomic_fetch_xor((u64*) (a), v, __ATOMIC_SEQ_CST)
+)w2c_template"
+R"w2c_template(
+#define atomic_exchange_u8(a, v) __atomic_exchange_n((u8*) (a), v, __ATOMIC_SEQ_CST)
+)w2c_template"
+R"w2c_template(#define atomic_exchange_u16(a, v) __atomic_exchange_n((u16*) (a), v, __ATOMIC_SEQ_CST)
+)w2c_template"
+R"w2c_template(#define atomic_exchange_u32(a, v) __atomic_exchange_n((u32*) (a), v, __ATOMIC_SEQ_CST)
+)w2c_template"
+R"w2c_template(#define atomic_exchange_u64(a, v) __atomic_exchange_n((u64*) (a), v, __ATOMIC_SEQ_CST)
+)w2c_template"
+R"w2c_template(
+#define atomic_compare_exchange_u8(a, expected_ptr, desired) __atomic_compare_exchange_n((u8*) (a), expected_ptr, desired, 0 /* is_weak */, __ATOMIC_SEQ_CST, __ATOMIC_SEQ_CST), *(expected_ptr)
+)w2c_template"
+R"w2c_template(#define atomic_compare_exchange_u16(a, expected_ptr, desired) __atomic_compare_exchange_n((u16*) (a), expected_ptr, desired, 0 /* is_weak */, __ATOMIC_SEQ_CST, __ATOMIC_SEQ_CST), *(expected_ptr)
+)w2c_template"
+R"w2c_template(#define atomic_compare_exchange_u32(a, expected_ptr, desired) __atomic_compare_exchange_n((u32*) (a), expected_ptr, desired, 0 /* is_weak */, __ATOMIC_SEQ_CST, __ATOMIC_SEQ_CST), *(expected_ptr)
+)w2c_template"
+R"w2c_template(#define atomic_compare_exchange_u64(a, expected_ptr, desired) __atomic_compare_exchange_n((u64*) (a), expected_ptr, desired, 0 /* is_weak */, __ATOMIC_SEQ_CST, __ATOMIC_SEQ_CST), *(expected_ptr)
+)w2c_template"
+R"w2c_template(
+#define atomic_fence() __atomic_thread_fence(__ATOMIC_SEQ_CST)
+)w2c_template"
+R"w2c_template(
+#endif
 )w2c_template"
 R"w2c_template(
 #define ATOMIC_ALIGNMENT_CHECK(addr, t1) \
@@ -55,7 +216,7 @@ R"w2c_template(    t1 result;                                             \
 )w2c_template"
 R"w2c_template(    wasm_rt_memcpy(&result, &mem->data[addr], sizeof(t1)); \
 )w2c_template"
-R"w2c_template(    result = atomic_load((t1*)&mem->data[addr]);           \
+R"w2c_template(    result = atomic_load_##t1(&mem->data[addr]);           \
 )w2c_template"
 R"w2c_template(    wasm_asm("" ::"r"(result));                            \
 )w2c_template"
@@ -89,7 +250,7 @@ R"w2c_template(    ATOMIC_ALIGNMENT_CHECK(addr, t1);                            
 )w2c_template"
 R"w2c_template(    t1 wrapped = (t1)value;                                            \
 )w2c_template"
-R"w2c_template(    atomic_store((t1*)&mem->data[addr], wrapped);                      \
+R"w2c_template(    atomic_store_##t1(&mem->data[addr], wrapped);                      \
 )w2c_template"
 R"w2c_template(  }
 )w2c_template"
@@ -119,7 +280,7 @@ R"w2c_template(    ATOMIC_ALIGNMENT_CHECK(addr, t1);                            
 )w2c_template"
 R"w2c_template(    t1 wrapped = (t1)value;                                          \
 )w2c_template"
-R"w2c_template(    t1 ret = atomic_fetch_##op((t1*)&mem->data[addr], wrapped);      \
+R"w2c_template(    t1 ret = atomic_##op##_##t1(&mem->data[addr], wrapped);          \
 )w2c_template"
 R"w2c_template(    return (t2)ret;                                                  \
 )w2c_template"
@@ -201,36 +362,19 @@ R"w2c_template(DEFINE_ATOMIC_RMW(i64_atomic_rmw32_xor_u, xor, u32, u64)
 R"w2c_template(DEFINE_ATOMIC_RMW(i64_atomic_rmw_xor, xor, u64, u64)
 )w2c_template"
 R"w2c_template(
-#define DEFINE_ATOMIC_RMW_XCHG(name, t1, t2)                         \
+DEFINE_ATOMIC_RMW(i32_atomic_rmw8_xchg_u, exchange, u8, u32)
 )w2c_template"
-R"w2c_template(  static inline t2 name(wasm_rt_memory_t* mem, u64 addr, t2 value) { \
+R"w2c_template(DEFINE_ATOMIC_RMW(i32_atomic_rmw16_xchg_u, exchange, u16, u32)
 )w2c_template"
-R"w2c_template(    MEMCHECK(mem, addr, t1);                                         \
+R"w2c_template(DEFINE_ATOMIC_RMW(i32_atomic_rmw_xchg, exchange, u32, u32)
 )w2c_template"
-R"w2c_template(    ATOMIC_ALIGNMENT_CHECK(addr, t1);                                \
+R"w2c_template(DEFINE_ATOMIC_RMW(i64_atomic_rmw8_xchg_u, exchange, u8, u64)
 )w2c_template"
-R"w2c_template(    t1 wrapped = (t1)value;                                          \
+R"w2c_template(DEFINE_ATOMIC_RMW(i64_atomic_rmw16_xchg_u, exchange, u16, u64)
 )w2c_template"
-R"w2c_template(    t1 ret = atomic_exchange((t1*)&mem->data[addr], wrapped);        \
+R"w2c_template(DEFINE_ATOMIC_RMW(i64_atomic_rmw32_xchg_u, exchange, u32, u64)
 )w2c_template"
-R"w2c_template(    return (t2)ret;                                                  \
-)w2c_template"
-R"w2c_template(  }
-)w2c_template"
-R"w2c_template(
-DEFINE_ATOMIC_RMW_XCHG(i32_atomic_rmw8_xchg_u, u8, u32)
-)w2c_template"
-R"w2c_template(DEFINE_ATOMIC_RMW_XCHG(i32_atomic_rmw16_xchg_u, u16, u32)
-)w2c_template"
-R"w2c_template(DEFINE_ATOMIC_RMW_XCHG(i32_atomic_rmw_xchg, u32, u32)
-)w2c_template"
-R"w2c_template(DEFINE_ATOMIC_RMW_XCHG(i64_atomic_rmw8_xchg_u, u8, u64)
-)w2c_template"
-R"w2c_template(DEFINE_ATOMIC_RMW_XCHG(i64_atomic_rmw16_xchg_u, u16, u64)
-)w2c_template"
-R"w2c_template(DEFINE_ATOMIC_RMW_XCHG(i64_atomic_rmw32_xchg_u, u32, u64)
-)w2c_template"
-R"w2c_template(DEFINE_ATOMIC_RMW_XCHG(i64_atomic_rmw_xchg, u64, u64)
+R"w2c_template(DEFINE_ATOMIC_RMW(i64_atomic_rmw_xchg, exchange, u64, u64)
 )w2c_template"
 R"w2c_template(
 #define DEFINE_ATOMIC_CMP_XCHG(name, t1, t2)                                 \
@@ -247,13 +391,11 @@ R"w2c_template(    t2 expected_wrapped = (t2)expected;                          
 )w2c_template"
 R"w2c_template(    t2 replacement_wrapped = (t2)replacement;                                \
 )w2c_template"
-R"w2c_template(    atomic_compare_exchange_strong((t2*)&mem->data[addr], &expected_wrapped, \
+R"w2c_template(    t2 old = atomic_compare_exchange_##t2(&mem->data[addr], &expected_wrapped,        \
 )w2c_template"
 R"w2c_template(                                   replacement_wrapped);                     \
 )w2c_template"
-R"w2c_template(    t2 ret = expected_wrapped /* expected_wrapped now has the old value */;  \
-)w2c_template"
-R"w2c_template(    return (t1)ret;                                                          \
+R"w2c_template(    return (t1)old;                                                          \
 )w2c_template"
 R"w2c_template(  }
 )w2c_template"
@@ -271,12 +413,5 @@ R"w2c_template(DEFINE_ATOMIC_CMP_XCHG(i64_atomic_rmw16_cmpxchg_u, u64, u16);
 R"w2c_template(DEFINE_ATOMIC_CMP_XCHG(i64_atomic_rmw32_cmpxchg_u, u64, u32);
 )w2c_template"
 R"w2c_template(DEFINE_ATOMIC_CMP_XCHG(i64_atomic_rmw_cmpxchg, u64, u64);
-)w2c_template"
-R"w2c_template(
-static inline void atomic_fence() {
-)w2c_template"
-R"w2c_template(  __atomic_thread_fence(__ATOMIC_SEQ_CST);
-)w2c_template"
-R"w2c_template(}
 )w2c_template"
 ;
--- src/template/wasm2c_atomicops.declarations.c
+++ src/template/wasm2c_atomicops.declarations.c
@@ -1,21 +1,112 @@
-#if __STDC_VERSION__ >= 201112L
+#if defined(_MSC_VER)
 
-#include <stdatomic.h>
+#include <intrin.h>
+
+// Use MSVC intrinsics
+
+// For loads and stores, its not clear if we can rely on register width loads
+// and stores to be atomic as reported here
+// https://learn.microsoft.com/en-us/windows/win32/sync/interlocked-variable-access?redirectedfrom=MSDN
+// or if we have to reuse other instrinsics
+// https://stackoverflow.com/questions/42660091/atomic-load-in-c-with-msvc
+// We reuse other intrinsics to be cautious
+#define atomic_load_u8(a, v) _InterlockedOr8(a, 0)
+#define atomic_load_u16(a, v) _InterlockedOr16(a, 0)
+#define atomic_load_u32(a, v) _InterlockedOr(a, 0)
+#define atomic_load_u64(a, v) _InterlockedOr64(a, 0)
+
+#define atomic_store_u8(a, v) _InterlockedExchange8(a, v)
+#define atomic_store_u16(a, v) _InterlockedExchange16(a, v)
+#define atomic_store_u32(a, v) _InterlockedExchange(a, v)
+#define atomic_store_u64(a, v) _InterlockedExchange64(a, v)
+
+#define atomic_add_u8(a, v) _InterlockedExchangeAdd8(a, v)
+#define atomic_add_u16(a, v) _InterlockedExchangeAdd16(a, v)
+#define atomic_add_u32(a, v) _InterlockedExchangeAdd(a, v)
+#define atomic_add_u64(a, v) _InterlockedExchangeAdd64(a, v)
+
+#define atomic_sub_u8(a, v) _InterlockedExchangeAdd8(a, -(v))
+#define atomic_sub_u16(a, v) _InterlockedExchangeAdd16(a, -(v))
+#define atomic_sub_u32(a, v) _InterlockedExchangeAdd(a, -(v))
+#define atomic_sub_u64(a, v) _InterlockedExchangeAdd64(a, -(v))
+
+#define atomic_and_u8(a, v) _InterlockedAnd8(a, v)
+#define atomic_and_u16(a, v) _InterlockedAnd16(a, v)
+#define atomic_and_u32(a, v) _InterlockedAnd(a, v)
+#define atomic_and_u64(a, v) _InterlockedAnd64(a, v)
+
+#define atomic_or_u8(a, v) _InterlockedOr8(a, v)
+#define atomic_or_u16(a, v) _InterlockedOr16(a, v)
+#define atomic_or_u32(a, v) _InterlockedOr(a, v)
+#define atomic_or_u64(a, v) _InterlockedOr64(a, v)
+
+#define atomic_xor_u8(a, v) _InterlockedXor8(a, v)
+#define atomic_xor_u16(a, v) _InterlockedXor16(a, v)
+#define atomic_xor_u32(a, v) _InterlockedXor(a, v)
+#define atomic_xor_u64(a, v) _InterlockedXor64(a, v)
+
+#define atomic_exchange_u8(a, v) _InterlockedExchange8(a, v)
+#define atomic_exchange_u16(a, v) _InterlockedExchange16(a, v)
+#define atomic_exchange_u32(a, v) _InterlockedExchange(a, v)
+#define atomic_exchange_u64(a, v) _InterlockedExchange64(a, v)
+
+#define atomic_compare_exchange_u8(a, expected_ptr, desired) _InterlockedCompareExchange8(a, desired, *(expected_ptr))
+#define atomic_compare_exchange_u16(a, expected_ptr, desired) _InterlockedCompareExchange16(a, desired, *(expected_ptr))
+#define atomic_compare_exchange_u32(a, expected_ptr, desired) _InterlockedCompareExchange(a, desired, *(expected_ptr))
+#define atomic_compare_exchange_u64(a, expected_ptr, desired) _InterlockedCompareExchange64(a, desired, *(expected_ptr))
+
+#define atomic_fence() _ReadWriteBarrier()
 
 #else
 
 // Use gcc/clang/icc intrinsics
-#define atomic_load(a) __atomic_load_n(a, __ATOMIC_SEQ_CST)
-#define atomic_store(a, v) __atomic_store_n(a, v, __ATOMIC_SEQ_CST)
-#define atomic_fetch_add(a, v) __atomic_fetch_add(a, v, __ATOMIC_SEQ_CST)
-#define atomic_fetch_sub(a, v) __atomic_fetch_sub(a, v, __ATOMIC_SEQ_CST)
-#define atomic_fetch_and(a, v) __atomic_fetch_and(a, v, __ATOMIC_SEQ_CST)
-#define atomic_fetch_or(a, v) __atomic_fetch_or(a, v, __ATOMIC_SEQ_CST)
-#define atomic_fetch_xor(a, v) __atomic_fetch_xor(a, v, __ATOMIC_SEQ_CST)
-#define atomic_exchange(a, v) __atomic_exchange_n(a, v, __ATOMIC_SEQ_CST)
-#define atomic_compare_exchange_strong(a, expected_ptr, desired)         \
-  __atomic_compare_exchange_n(a, expected_ptr, desired, 0 /* is_weak */, \
-                              __ATOMIC_SEQ_CST, __ATOMIC_SEQ_CST)
+#define atomic_load_u8(a) __atomic_load_n((u8*) (a), __ATOMIC_SEQ_CST)
+#define atomic_load_u16(a) __atomic_load_n((u16*) (a), __ATOMIC_SEQ_CST)
+#define atomic_load_u32(a) __atomic_load_n((u32*) (a), __ATOMIC_SEQ_CST)
+#define atomic_load_u64(a) __atomic_load_n((u64*) (a), __ATOMIC_SEQ_CST)
+
+#define atomic_store_u8(a, v) __atomic_store_n((u8*) (a), v, __ATOMIC_SEQ_CST)
+#define atomic_store_u16(a, v) __atomic_store_n((u16*) (a), v, __ATOMIC_SEQ_CST)
+#define atomic_store_u32(a, v) __atomic_store_n((u32*) (a), v, __ATOMIC_SEQ_CST)
+#define atomic_store_u64(a, v) __atomic_store_n((u64*) (a), v, __ATOMIC_SEQ_CST)
+
+#define atomic_add_u8(a, v) __atomic_fetch_add((u8*) (a), v, __ATOMIC_SEQ_CST)
+#define atomic_add_u16(a, v) __atomic_fetch_add((u16*) (a), v, __ATOMIC_SEQ_CST)
+#define atomic_add_u32(a, v) __atomic_fetch_add((u32*) (a), v, __ATOMIC_SEQ_CST)
+#define atomic_add_u64(a, v) __atomic_fetch_add((u64*) (a), v, __ATOMIC_SEQ_CST)
+
+#define atomic_sub_u8(a, v) __atomic_fetch_sub((u8*) (a), v, __ATOMIC_SEQ_CST)
+#define atomic_sub_u16(a, v) __atomic_fetch_sub((u16*) (a), v, __ATOMIC_SEQ_CST)
+#define atomic_sub_u32(a, v) __atomic_fetch_sub((u32*) (a), v, __ATOMIC_SEQ_CST)
+#define atomic_sub_u64(a, v) __atomic_fetch_sub((u64*) (a), v, __ATOMIC_SEQ_CST)
+
+#define atomic_and_u8(a, v) __atomic_fetch_and((u8*) (a), v, __ATOMIC_SEQ_CST)
+#define atomic_and_u16(a, v) __atomic_fetch_and((u16*) (a), v, __ATOMIC_SEQ_CST)
+#define atomic_and_u32(a, v) __atomic_fetch_and((u32*) (a), v, __ATOMIC_SEQ_CST)
+#define atomic_and_u64(a, v) __atomic_fetch_and((u64*) (a), v, __ATOMIC_SEQ_CST)
+
+#define atomic_or_u8(a, v) __atomic_fetch_or((u8*) (a), v, __ATOMIC_SEQ_CST)
+#define atomic_or_u16(a, v) __atomic_fetch_or((u16*) (a), v, __ATOMIC_SEQ_CST)
+#define atomic_or_u32(a, v) __atomic_fetch_or((u32*) (a), v, __ATOMIC_SEQ_CST)
+#define atomic_or_u64(a, v) __atomic_fetch_or((u64*) (a), v, __ATOMIC_SEQ_CST)
+
+#define atomic_xor_u8(a, v) __atomic_fetch_xor((u8*) (a), v, __ATOMIC_SEQ_CST)
+#define atomic_xor_u16(a, v) __atomic_fetch_xor((u16*) (a), v, __ATOMIC_SEQ_CST)
+#define atomic_xor_u32(a, v) __atomic_fetch_xor((u32*) (a), v, __ATOMIC_SEQ_CST)
+#define atomic_xor_u64(a, v) __atomic_fetch_xor((u64*) (a), v, __ATOMIC_SEQ_CST)
+
+#define atomic_exchange_u8(a, v) __atomic_exchange_n((u8*) (a), v, __ATOMIC_SEQ_CST)
+#define atomic_exchange_u16(a, v) __atomic_exchange_n((u16*) (a), v, __ATOMIC_SEQ_CST)
+#define atomic_exchange_u32(a, v) __atomic_exchange_n((u32*) (a), v, __ATOMIC_SEQ_CST)
+#define atomic_exchange_u64(a, v) __atomic_exchange_n((u64*) (a), v, __ATOMIC_SEQ_CST)
+
+#define atomic_compare_exchange_u8(a, expected_ptr, desired) __atomic_compare_exchange_n((u8*) (a), expected_ptr, desired, 0 /* is_weak */, __ATOMIC_SEQ_CST, __ATOMIC_SEQ_CST), *(expected_ptr)
+#define atomic_compare_exchange_u16(a, expected_ptr, desired) __atomic_compare_exchange_n((u16*) (a), expected_ptr, desired, 0 /* is_weak */, __ATOMIC_SEQ_CST, __ATOMIC_SEQ_CST), *(expected_ptr)
+#define atomic_compare_exchange_u32(a, expected_ptr, desired) __atomic_compare_exchange_n((u32*) (a), expected_ptr, desired, 0 /* is_weak */, __ATOMIC_SEQ_CST, __ATOMIC_SEQ_CST), *(expected_ptr)
+#define atomic_compare_exchange_u64(a, expected_ptr, desired) __atomic_compare_exchange_n((u64*) (a), expected_ptr, desired, 0 /* is_weak */, __ATOMIC_SEQ_CST, __ATOMIC_SEQ_CST), *(expected_ptr)
+
+#define atomic_fence() __atomic_thread_fence(__ATOMIC_SEQ_CST)
+
 #endif
 
 #define ATOMIC_ALIGNMENT_CHECK(addr, t1) \
@@ -29,7 +120,7 @@
     ATOMIC_ALIGNMENT_CHECK(addr, t1);                      \
     t1 result;                                             \
     wasm_rt_memcpy(&result, &mem->data[addr], sizeof(t1)); \
-    result = atomic_load((t1*)&mem->data[addr]);           \
+    result = atomic_load_##t1(&mem->data[addr]);           \
     wasm_asm("" ::"r"(result));                            \
     return (t3)(t2)result;                                 \
   }
@@ -47,7 +138,7 @@ DEFINE_ATOMIC_LOAD(i64_atomic_load32_u, u32, u64, u64)
     MEMCHECK(mem, addr, t1);                                           \
     ATOMIC_ALIGNMENT_CHECK(addr, t1);                                  \
     t1 wrapped = (t1)value;                                            \
-    atomic_store((t1*)&mem->data[addr], wrapped);                      \
+    atomic_store_##t1(&mem->data[addr], wrapped);                      \
   }
 
 DEFINE_ATOMIC_STORE(i32_atomic_store, u32, u32)
@@ -63,7 +154,7 @@ DEFINE_ATOMIC_STORE(i64_atomic_store32, u32, u64)
     MEMCHECK(mem, addr, t1);                                         \
     ATOMIC_ALIGNMENT_CHECK(addr, t1);                                \
     t1 wrapped = (t1)value;                                          \
-    t1 ret = atomic_fetch_##op((t1*)&mem->data[addr], wrapped);      \
+    t1 ret = atomic_##op##_##t1(&mem->data[addr], wrapped);          \
     return (t2)ret;                                                  \
   }
 
@@ -107,22 +198,13 @@ DEFINE_ATOMIC_RMW(i64_atomic_rmw16_xor_u, xor, u16, u64)
 DEFINE_ATOMIC_RMW(i64_atomic_rmw32_xor_u, xor, u32, u64)
 DEFINE_ATOMIC_RMW(i64_atomic_rmw_xor, xor, u64, u64)
 
-#define DEFINE_ATOMIC_RMW_XCHG(name, t1, t2)                         \
-  static inline t2 name(wasm_rt_memory_t* mem, u64 addr, t2 value) { \
-    MEMCHECK(mem, addr, t1);                                         \
-    ATOMIC_ALIGNMENT_CHECK(addr, t1);                                \
-    t1 wrapped = (t1)value;                                          \
-    t1 ret = atomic_exchange((t1*)&mem->data[addr], wrapped);        \
-    return (t2)ret;                                                  \
-  }
-
-DEFINE_ATOMIC_RMW_XCHG(i32_atomic_rmw8_xchg_u, u8, u32)
-DEFINE_ATOMIC_RMW_XCHG(i32_atomic_rmw16_xchg_u, u16, u32)
-DEFINE_ATOMIC_RMW_XCHG(i32_atomic_rmw_xchg, u32, u32)
-DEFINE_ATOMIC_RMW_XCHG(i64_atomic_rmw8_xchg_u, u8, u64)
-DEFINE_ATOMIC_RMW_XCHG(i64_atomic_rmw16_xchg_u, u16, u64)
-DEFINE_ATOMIC_RMW_XCHG(i64_atomic_rmw32_xchg_u, u32, u64)
-DEFINE_ATOMIC_RMW_XCHG(i64_atomic_rmw_xchg, u64, u64)
+DEFINE_ATOMIC_RMW(i32_atomic_rmw8_xchg_u, exchange, u8, u32)
+DEFINE_ATOMIC_RMW(i32_atomic_rmw16_xchg_u, exchange, u16, u32)
+DEFINE_ATOMIC_RMW(i32_atomic_rmw_xchg, exchange, u32, u32)
+DEFINE_ATOMIC_RMW(i64_atomic_rmw8_xchg_u, exchange, u8, u64)
+DEFINE_ATOMIC_RMW(i64_atomic_rmw16_xchg_u, exchange, u16, u64)
+DEFINE_ATOMIC_RMW(i64_atomic_rmw32_xchg_u, exchange, u32, u64)
+DEFINE_ATOMIC_RMW(i64_atomic_rmw_xchg, exchange, u64, u64)
 
 #define DEFINE_ATOMIC_CMP_XCHG(name, t1, t2)                                 \
   static inline t1 name(wasm_rt_memory_t* mem, u64 addr, t1 expected,        \
@@ -131,10 +213,9 @@ DEFINE_ATOMIC_RMW_XCHG(i64_atomic_rmw_xchg, u64, u64)
     ATOMIC_ALIGNMENT_CHECK(addr, t2);                                        \
     t2 expected_wrapped = (t2)expected;                                      \
     t2 replacement_wrapped = (t2)replacement;                                \
-    atomic_compare_exchange_strong((t2*)&mem->data[addr], &expected_wrapped, \
+    t2 old = atomic_compare_exchange_##t2(&mem->data[addr], &expected_wrapped,        \
                                    replacement_wrapped);                     \
-    t2 ret = expected_wrapped /* expected_wrapped now has the old value */;  \
-    return (t1)ret;                                                          \
+    return (t1)old;                                                          \
   }
 
 DEFINE_ATOMIC_CMP_XCHG(i32_atomic_rmw8_cmpxchg_u, u32, u8);
@@ -144,7 +225,3 @@ DEFINE_ATOMIC_CMP_XCHG(i64_atomic_rmw8_cmpxchg_u, u64, u8);
 DEFINE_ATOMIC_CMP_XCHG(i64_atomic_rmw16_cmpxchg_u, u64, u16);
 DEFINE_ATOMIC_CMP_XCHG(i64_atomic_rmw32_cmpxchg_u, u64, u32);
 DEFINE_ATOMIC_CMP_XCHG(i64_atomic_rmw_cmpxchg, u64, u64);
-
-static inline void atomic_fence() {
-  __atomic_thread_fence(__ATOMIC_SEQ_CST);
-}
